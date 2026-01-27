b253ad99ae10f3e765ab63f7c8c3df2c3e8855cb
thedirector <cm@variance.digital>
2026-01-27 18:13:38 -0500

    Initial backend server

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post("/voice", (req, res) => {
+  res.type("text/xml");
+  res.send(`
+    <Response>
+      <Say>
+        Hello. This is the AI intake assistant.
+      </Say>
+    </Response>
+  `);
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend is live.");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
